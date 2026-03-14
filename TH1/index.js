77b7077967c1b5ea05d7025b9503288ae5d8b22c
ngan <daihock46@gmail.com>
2026-03-14 13:09:54 +0000

    BAI THUC HANH SO 1

---
+++ TH1/index.js
@@ -0,0 +1,29 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+
+    if(req.url === "/"){
+        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'})
+        res.end("Trang chủ")
+    }
+
+    else if(req.url === "/about"){
+        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'})
+        res.end("Trang giới thiệu")
+    }
+
+    else if(req.url === "/contact"){
+        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'})
+        res.end("Trang liên hệ")
+    }
+
+    else{
+        res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'})
+        res.end("Không tìm thấy trang")
+    }
+
+})
+
+server.listen(3000, () => {
+    console.log("Server đang chạy tại port 3000")
+})
