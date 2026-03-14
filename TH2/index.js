7dc4ea3c16babad8cbbfa2276a3ffb794667add2
ngan <daihock46@gmail.com>
2026-03-14 13:21:49 +0000

    update

---
+++ TH2/index.js
@@ -0,0 +1,13 @@
+const express = require('express') 
+const app = new express() 
+const path = require('path') 
+
+app.use(express.static('public')) 
+
+app.get('/', (request, response) =>{
+    response.sendFile(path.resolve(__dirname, 'pages/index.html')) 
+})
+
+app.listen(4000, ()=>{ 
+    console.log('App listening on port 4000')
+})
